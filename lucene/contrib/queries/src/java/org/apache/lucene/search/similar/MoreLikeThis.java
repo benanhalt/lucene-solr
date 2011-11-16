fe92682bb832317583269c718012c974d7681392
Ben Anhalt <anhalt@ku.edu>
2011-11-16 14:59:28 -0600

    fix missing import

--- lucene/contrib/queries/src/java/org/apache/lucene/search/similar/MoreLikeThis.java
+++ lucene/contrib/queries/src/java/org/apache/lucene/search/similar/MoreLikeThis.java
@@ -25,6 +25,7 @@ import java.io.StringReader;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
